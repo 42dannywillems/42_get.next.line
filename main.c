6976be5e49c35a019301a2add89c61b51babd1f6
Danny Willems <dan.wil@hotmail.com>
2015-01-29 20:23:37 +0100

    default message

--- main.c
+++ main.c
@@ -5,24 +5,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define NB_LINE 50
 int main(int argc, char **argv)
 {
 	int fd;
-	char **line;
+	int fd2;
+	char *line;
+	count_t i;
 
-	if ((line = (char **)malloc(sizeof(char *))) && 
-		(fd = open("get_next_line.c", O_RDONLY)) != -1)
+	i = 0;
+	if ((fd = open("get_next_line.c", O_RDONLY)) != -1 &&
+		(fd2 = open("get_next_line.h", O_RDONLY)) != -1)
+		
 	{
-		get_next_line(fd, line);
-//		ft_putendl(*line);
-		get_next_line(fd, line);
-//		ft_putendl(*line);
-		get_next_line(fd, line);
-//		ft_putendl(*line);
-		get_next_line(fd, line);
-//		ft_putendl(*line);
-		get_next_line(fd, line);
-//		ft_putendl(*line);
+		while (get_next_line(fd, &line) > 0 && i < NB_LINE)
+		{
+			ft_putstr("Ligne ");
+			ft_putnbrendl(i);
+			ft_putendl(line);
+			ft_strdel(&line);
+			i++;
+		}
 	}
 	return (0);
 }
