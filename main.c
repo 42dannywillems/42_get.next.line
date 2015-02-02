9dc0a88880cf4f64ad637f5d0042a97e1a34a78c
Danny Willems <dan.wil@hotmail.com>
2015-02-02 22:26:02 +0100

    default message

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define NB_LINE 500
+#define NB_LINE 1
 int main(int argc, char **argv)
 {
-	int fd, fd2;
-	char *line;
-	count_t i;
-	char buf[6];
+	int		fd;
+	int		fd2;
+	int		ret;
+	count_t	i;
+	char	*line;
+	char	buf[6];
 
 	if (argc >= 2 &&	(fd = open(argv[1], O_RDONLY)) != -1 &&
 						(fd2 = open(argv[2], O_RDONLY)) != -1)
@@ -21,17 +23,23 @@ int main(int argc, char **argv)
 		{
 //			ft_putstr("Ligne ");
 //			ft_putnbrendl(i);
-			if (get_next_line(fd, &line) > 0)
+			if ((ret = get_next_line(fd, &line)) > 0)
 				ft_putendl(line);
+			ft_putnstr("*", 80);
+			ft_putendl("");
+			ft_putnbrendl(ret);
 			ft_strdel(&line);
 
 //			read(STDIN, buf, 5);
-			if (get_next_line(fd2, &line) > 0)
+			if ((ret = get_next_line(fd2, &line)) > 0)
 				ft_putendl(line);
+			ft_putnbrendl(ret);
 			ft_strdel(&line);
 
 //			read(STDIN, buf, 5);
 			i++;
+//			ft_putnstr("-", 80);
+//			ft_putendl("");
 		}
 		close(fd);
 		close(fd2);
